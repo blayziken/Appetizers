8bf89fc45df5986f008a485011801a3087152793
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-12-16 08:03:33 +0100

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Blaze on 16/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
