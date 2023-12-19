6a727682c2cf1e426d8d0fb3ad549e8b3a28cfdb
Toluwalope Oluyipe <kingtoluwalope@gmail.com>
2023-12-19 16:45:21 +0100

    Working on detail view

---
+++ Appetizers/Views/Home/DetailView.swift
@@ -0,0 +1,104 @@
+//
+//  DetailView.swift
+//  Appetizers
+//
+//  Created by Blaze on 19/12/2023.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let appetizer: Appetizer
+    
+    var body: some View {
+        VStack {
+            AppetizerRemoteImage(urlString: appetizer.imageURL)
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 320, height: 225)
+                .background(.red)
+            
+            
+            VStack {
+                Text(appetizer.name)
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                
+                Text(appetizer.description)
+                    .multilineTextAlignment(.center)
+                    .font(.body)
+                    .padding()
+                
+                HStack(spacing: 40) {
+                    NutritionInfo(title: "Calories", value: "\(appetizer.calories)")
+                    NutritionInfo(title: "Carbs", value: "\(appetizer.carbs) g")
+                    NutritionInfo(title: "Protein", value: "\(appetizer.protein) g")
+                }
+                Spacer()
+                
+                Button {
+                    
+                }label: {
+                    
+                    Text("$\(appetizer.price, specifier: "%.2f") - Add to Order")
+                }
+                //                .modifier(StandardButtonStyle())
+                .buttonStyle(.borderedProminent)
+                .controlSize(.large)
+                .padding(.bottom, 30)
+            }
+        }
+        .frame(width: 320, height: 525)
+        .background(Color(.systemBackground))
+        .cornerRadius(12)
+        .shadow(radius: 40)
+        .overlay(alignment: .topTrailing) {
+            Button {
+                
+            } label: {
+                XDismissButton()
+            }
+        }
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(appetizer: MockData.sample)
+    }
+}
+
+struct NutritionInfo: View {
+    
+    let title: String
+    let value: String
+    
+    var body: some View {
+        VStack(spacing: 5) {
+            Text(title)
+                .bold()
+                .font(.caption)
+            
+            Text(value)
+                .foregroundColor(.secondary)
+                .fontWeight(.semibold)
+                .italic()
+        }
+    }
+}
+
+
+struct XDismissButton: View {
+    var body: some View {
+        ZStack {
+            Circle()
+                .frame(width: 30, height: 30)
+                .foregroundColor(.white)
+                .opacity(0.6)
+            
+            Image(systemName: "xmark")
+                .imageScale(.small)
+                .frame(width: 44, height: 44)
+                .foregroundColor(.black)
+        }
+    }
+}
